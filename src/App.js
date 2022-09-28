bbf4de593ccbb22ddb4bd6864c5009385258702b
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-28 14:31:48 +0530

    initial commit of App and List components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  return (
+    <main>
+      <section className='container'>
+       
+      </section>
+    </main>
+  );
+}
+
+export default App;
